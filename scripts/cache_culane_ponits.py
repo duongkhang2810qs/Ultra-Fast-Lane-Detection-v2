70f695a9a1c55024ac11a6e6b077afec659f6eb1
Le Duong Khang <duongkhang2810qs@gmail.com>
2025-09-05 22:44:33 +0700

    Update cache_culane_ponits.py

--- scripts/cache_culane_ponits.py
+++ scripts/cache_culane_ponits.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
         txt_path = os.path.join(culane_root, txt_path)
         lanes = open(txt_path, 'r').readlines()
 
-        all_points = np.zeros((4,35,2), dtype=np.float)
+        all_points = np.zeros((4,35,2), dtype=np.float32)
         the_anno_row_anchor = np.array([250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590])
         all_points[:,:,1] = np.tile(the_anno_row_anchor, (4,1))
         all_points[:,:,0] = -99999
